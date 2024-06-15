905036f015ec0630fe52d6c723c5f6323440932b
Tioluwani <codemeastro101@gmail.com>
2024-06-15 01:04:04 +0100

    worked on the uuser interface for the contact form

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -5,6 +5,7 @@ import Input from "../components/Input";
 export default function Contact() {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const HANDLE_SUBMIT = async (e) => {
     e.preventDefault();
@@ -25,25 +26,33 @@ export default function Contact() {
     }
 
     try {
-      const RESPONSE = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/v1/mail` , {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          phoneno: phoneno,
-          message: message,
-        }),
-      });
+      setLoading(true);
+      const RESPONSE = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/api/v1/mail`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: name,
+            email: email,
+            phoneno: phoneno,
+            message: message,
+          }),
+        }
+      );
 
       const DATA = await RESPONSE.json();
-      setSuccess(DATA.message);
-
-      throw DATA.error;
+      if(DATA.message) {
+        setLoading(false)
+        return setSuccess(DATA.message);
+      } else {
+        throw DATA.error;
+      }
     } catch (error) {
-      if(error) {
+      if (error) {
+        setLoading(false);
         console.error(error);
         setError(error);
       }
@@ -52,12 +61,15 @@ export default function Contact() {
 
   return (
     <div className="flex flex-col items-center justify-between h-full bg-[#101017] pt-[50px] pb-[120px] font-mono">
-      <div className="flex flex-col items-center gap-10">
+      <div className="flex flex-col items-center gap-10 w-full md:w-96">
         <h1 className="text-[#079211] font-bold text-3xl mt-32">
           Send me a message.
         </h1>
-        <p className="text-[#079211]">{success}</p>
-        <form className="flex flex-col gap-5 w-full" onSubmit={HANDLE_SUBMIT}>
+        <p className="text-[#079211] text-center w-full px-[10px]">{success}</p>
+        <form
+          className="flex flex-col gap-5 w-full px-[10px]"
+          onSubmit={HANDLE_SUBMIT}
+        >
           <Input type="text" name="name" placeholder="Your Full Name" />
           <Input type="name" name="email" placeholder="Your Email" />
           <Input type="number" name="phoneno" placeholder="Your Phone Number" />
@@ -65,10 +77,20 @@ export default function Contact() {
             placeholder="Your Message"
             name="message"
             id="message"
-            className="bg-[#2B2B2B] focus:bg-slate-500 ps-5 py-3 w-full md:w-96 rounded-md text-white"
+            className="bg-[#2B2B2B] focus:bg-slate-500 ps-5 py-3 w-full rounded-md text-white"
           ></textarea>
-          <Input type="submit" value="Contact Me" />
-          <p className="text-red-500">{error}</p>
+          {/* <Input type="submit" value="Contact Me" /> */}
+          <button
+            type="submit"
+            className="bg-[#079211] focus:bg-slate-500 py-3 w-full rounded-md text-white cursor-pointer hover:bg-slate-500 transition-all uppercase font-bold"
+          >
+            {loading ? (
+              <div className="ease-linear rounded-full border-4 border-t-4 border-t-white border-[#101017] h-10 w-10 animate-spin ms-[160px]"></div>
+            ) : (
+              "Contact Me"
+            )}
+          </button>
+          <p className="text-red-500 text-center">{error}</p>
         </form>
       </div>
     </div>
