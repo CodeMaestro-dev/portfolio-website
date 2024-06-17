f9c534511806d4c70edee70452ea67a29440f125
Tioluwani <codemeastro101@gmail.com>
2024-06-17 21:25:00 +0100

    worked on the harmburger menu and cv document.

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -7,8 +7,6 @@ export default function Navbar() {
     setShow(!show);
   };
 
-
-
   return (
     <div className="bg-[#101017] px-[20px] lg:px-[123px] py-[23px] text-white font-bold flex flex-col items-center gap-[20px] md:gap-0 md:flex-row fixed w-full justify-between font-mono">
       <div className="flex items-center justify-between w-full">
@@ -17,20 +15,37 @@ export default function Navbar() {
         </Link>
 
         <div className="block md:hidden cursor-pointer" onClick={SET_SHOW}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+          {!show ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          )}
         </div>
       </div>
 
